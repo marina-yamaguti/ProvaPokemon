c3b9c53c209d7977e974647ff4dbfd4735036b14
Marina Geller Yamaguti <111259549+marina-yamaguti@users.noreply.github.com>
2022-09-05 12:31:47 -0300

    Update SequenceQuestion.java

--- SequenceQuestion.java
+++ SequenceQuestion.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
 public class SequenceQuestion extends Question {
-    
-    
-    
-    
-    public void display(){
+    private ArrayList<String> sequences;
+
+    public SequenceQuestion(){
+        sequences = new ArrayList<String>();
+    }
+
+    public void addSequence (String sequence){
+        sequences.add(sequence);
+    }
+
+
+    public void display()
+    {
         System.out.print("Digite a sequência correspondente à sequência que responde corretamente: ");
         super.display();
-
 
     }
 }
