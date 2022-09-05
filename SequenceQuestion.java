24ed77d92ce2881893f5ffdb503ac9211d7a06f5
Giovanni Schardong <100441428+gioschardong@users.noreply.github.com>
2022-09-05 10:34:54 -0300

    Update SequenceQuestion.java

--- SequenceQuestion.java
+++ SequenceQuestion.java
@@ -2,7 +2,7 @@ public class SequenceQuestion extends Question {
     
     public void display()
     {
-        System.out.print("Digite a alternativa correspondente à sequência que responde corretamente: ");
+        System.out.print("Digite a sequencia correspondente à sequência que responde corretamente: ");
         super.display();
 
 
